cc8f2bbcede9bd87f70ddd109cac7f8639ef2ad1
Sam Ngigi <sam.t.ngigi@gmail.com>
2018-03-29 02:54:20 +0300

    fetch working

--- post_app/views.py
+++ post_app/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
 
 
 def post_detail_view(request, id=1):
-    return render(request, 'posts/post_detail.html', {})
+    obj = Post.objects.get(id=id)
+    content = {
+        "objects": obj,
+    }
+    return render(request, 'posts/post_detail.html', content)
 
 
 def post_list_view(request):
-    return render(request, 'posts/post_list.html', {})
+    queryset = Post.objects.all()
+
+    content = {
+        "object_list": queryset,
+    }
+    return render(request, 'posts/post_list.html', content)
