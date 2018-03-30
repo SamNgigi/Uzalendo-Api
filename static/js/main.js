85f5586e8ced57b2eda2d687f3ba7fb8a68d1c4b
Sam Ngigi <sam.t.ngigi@gmail.com>
2018-03-30 23:34:15 +0300

    rendered ajax response! Working but messy

--- static/js/main.js
+++ static/js/main.js
@@ -1,17 +1,33 @@
+// Note:
+// "{% url 'posts_api:post_list_api' %}" would work if we working with a block script. i.e in base.html
+// However with an external script file we have to write out the url the old fashion way
+
 $(document).ready(function() {
   console.log("Working");
   $.ajax({
     url:"/posts/api/",
     method:"GET",
     success: function(data) {
+      // Loging if call is successful
         console.log(data);
+        // Loging data in key value pair
+        $.each(data, function(key, value){
+          console.log(key);
+          console.log(value.user);
+          console.log(value.content);
+          // Storing data from ajax call.
+          var postKey = value.key;
+          var postUser = value.user;
+          var postContent = value.content;
+          // Ajax rendering of the call. Long and messy html definition
+          $("#post-container").append(
+            "<p>"+ "-" + postContent + "<br/>" + postUser.username + " | " + "<a href='#'>View</a>" +"</p>" + "<br/>" + "<hr>"
+          )
+        })
     },
     error: function(data){
       console.log("error");
       console.log(data);
     }
   })
-  // Note:
-  // "{% url 'posts_api:post_list_api' %}" would work if we working with a block script. i.e in base.html
-  // However with an external script file we have to write out the url the old fashion way
 });
