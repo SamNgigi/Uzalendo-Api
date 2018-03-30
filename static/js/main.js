18a3beaa49c8105af0311d6221e2eabdb5a12636
Sam Ngigi <sam.t.ngigi@gmail.com>
2018-03-30 22:39:45 +0300

    ajax call working

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
   console.log("Working");
+  $.ajax({
+    url:"/posts/api/",
+    method:"GET",
+    success: function(data) {
+        console.log(data);
+    },
+    error: function(data){
+      console.log("error");
+      console.log(data);
+    }
+  })
+  // Note:
+  // "{% url 'posts_api:post_list_api' %}" would work if we working with a block script. i.e in base.html
+  // However with an external script file we have to write out the url the old fashion way
 });
