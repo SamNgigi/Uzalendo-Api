56d645b52b3db4350deaa82be3bc01d13a7a17f8
Sam Ngigi <sam.t.ngigi@gmail.com>
2018-04-11 21:43:34 +0300

    Added bootstrap

--- src/components/Aminia.jsx
+++ src/components/Aminia.jsx
@@ -5,6 +5,14 @@ import { connect } from 'react-redux';
 
 import { posts } from "../actions";
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'jquery/dist/jquery.min.js';
+import 'popper.js/dist/umd/popper.min.js';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import {
+  Button, FormGroup, Form, Input, Container,
+} from 'reactstrap';
+
 class Aminia extends Component {
 
   state = {
@@ -39,36 +47,41 @@ class Aminia extends Component {
         <hr/>
 
         <p>Naskia kuzitoka!!</p>
-          <form onSubmit={this.submitPost}>
-            <input
-              value={this.state.content}
-              placeholder="Enter post here..."
-              onChange = {(event)=> this.setState({content:event.target.value})}
-              required
-            />
-            <button onClick={this.resetForm}>Reset</button>
-            <input type="submit" value="Save Post"/>
-          </form>
+          <Container>
+            <Form onSubmit={this.submitPost}>
+              <FormGroup>
+                <Input
+                  value={this.state.content}
+                  placeholder="Enter post here..."
+                  onChange = {(event)=> this.setState({content:event.target.value})}
+                  required
+                />
+                <Button outline className="m-1" onClick={this.resetForm} block>Reset</Button>
+                <Button outline className="m-1" type="submit" color="success" block>Save Post</Button>
+              </FormGroup>
+            </Form>
+          </Container>
 
 
         <p>
           <Link to="/contact">Click Here</Link> to contact us.
         </p>
         <h3>Posts</h3>
+
         <table>
           <tbody>
             {this.props.posts.map((post, id) => (
               <tr key={`post_${post.id}`}>
                 <td>{post.content}</td>
                 <td>
-                  <button onClick={() => this.selectForEdit(id)}>
+                  <Button outline color="prinary" onClick={() => this.selectForEdit(id)}>
                     edit
-                  </button>
+                  </Button>
                 </td>
                 <td>
-                  <button onClick={() => this.props.deletePost(id)}>
+                  <Button outline color="danger" onClick={() => this.props.deletePost(id)}>
                     delete
-                  </button>
+                  </Button>
                 </td>
               </tr>
             ))}
