2032dd5b1ea82b673d23e5a80917279c44882dd7
Sam Ngigi <sam.t.ngigi@gmail.com>
2018-04-11 09:59:01 +0300

    Adding navbar alllowing login and logout. Changed max chars to 30chars

--- static/js/main.js
+++ static/js/main.js
@@ -265,7 +265,7 @@ function loadPosts(postContainerId) {
     })
 
     // Char counting
-    var charsStart = 155;
+    var charsStart = 30;
     var charsCounter = 0;
     // Appending characters left counter to our create post form
     $("#post-form").append(
